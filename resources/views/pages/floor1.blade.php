31ad577dcac08d9ada2f16ad172932b4d831f3cc
kixmbap <pwathkim@gmail.com>
2025-09-10 00:58:15 +0700

    Profile + img ใส่ตัวอักรแทนรูป

--- resources/views/pages/floor1.blade.php
+++ resources/views/pages/floor1.blade.php
@@ -3,6 +3,6 @@
 @section('title', 'floor1')
 
 @section('content')
-  <br><br><br><br><br><br><br>
+  
   <h1 style="text-indent: 60px; font-size: 30px; text-decoration: underline;">ชั้น 1</h1><br>
 @endsection
--- resources/views/pages/floor2.blade.php
+++ resources/views/pages/floor2.blade.php
@@ -3,6 +3,5 @@
 @section('title', 'floor2')
 
 @section('content')
-  <br><br><br><br><br><br><br>
   <h1 style="text-indent: 60px; font-size: 30px; text-decoration: underline;">ชั้น 2</h1><br>
 @endsection
