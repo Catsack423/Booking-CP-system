203bdc72ef7a742129973abe86850b5377b97604
kixmbap <pwathkim@gmail.com>
2025-09-13 02:42:51 +0700

    Update ล่าสุดโบร

--- resources/views/pages/floor1.blade.php
+++ resources/views/pages/floor1.blade.php
@@ -3,13 +3,8 @@
 @section('title', 'ชั้น 1')
 <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/png">
 @section('content')
-  <title>ชั้น 1</title>
-  <h1 style="text-indent: 60px; font-size: 30px; text-decoration: underline;">ชั้น 1</h1><br>
-  {{-- testdata --}}
-  @foreach ($rooms as $data )
-    {{ $data }}
-  @endforeach
-  {{-- end --}}
+<h1 style="text-indent: 60px; font-size: 30px; text-decoration: underline;">ชั้น 1</h1><br>
+
 
 
   
