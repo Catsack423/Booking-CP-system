39c14b12512739d88c5cfe10bfe9178a1bb2da6c
Catsack423 <face11773@gmail.com>
2025-09-14 20:10:19 +0700

    Fixing Bug

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,16 +15,6 @@
     <a href="{{ route('profile') }}" class="btn">แก้ไขข้อมูลส่วนตัว</a>
     <a href="{{ route('HistoryBooking') }}" class="btn btn-primary">ดูประวัติการจอง</a>
   </div>
-<div class="profile-content">
-    <!-- Card 1: ข้อมูลส่วนตัว -->
-    <div class="card">
-        <div class="cardProfile-img">
-            <img src="{{ Auth::user()->profile_photo_url }}" alt="avatar" class="user-avatar">
-        </div>
-        <h2>แก้ไขข้อมูลส่วนตัว</h2>
-        @livewire('profile.update-profile-information-form')
-
-    </div>
     <div class="profile-content">
         <!-- Card 1: ข้อมูลส่วนตัว -->
         <div class="card">
